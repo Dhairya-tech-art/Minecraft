c2d3bc3cf23c7e9a50e82a7791680f368da770cf
Dhairya-tech-art <74977651+Dhairya-tech-art@users.noreply.github.com>
2020-12-14 20:53:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var canvas=new fabric.Canvas('myCanvas');
 block_image_width=30;
 block_image_height=30;
 player_x=10;
-player_y=10;
+player_y=10; 
 var player_object="";
 function player_update()
 {
@@ -106,5 +106,45 @@ function my_keydown(e){
     if(keyPressed=='67'){
         new_image('cloud.jpg');
         console.log("c");
+    }
+}
+function up(){
+    if(player_y>=0)
+    {
+        player_y=player_y-block_image_height;
+        console.log("block image height="+block_image_height);
+        console.log("when up arrow key is pressed,X="+player_x+",Y="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down(){
+    if(player_y<=500)
+    {
+        player_y=player_y+block_image_height;
+        console.log("block image height="+block_image_height);
+        console.log("when down arrow key is pressed,X="+player_x+",Y="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left(){
+    if(player_x>0)
+    {
+        player_x=player_x-block_image_width;
+        console.log("block image width="+block_image_width);
+        console.log("when left arrow key is pressed,X="+player_x+",Y="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right(){
+    if(player_x<=850)
+    {
+        player_x=player_x+block_image_width;
+        console.log("block image width="+block_image_width);
+        console.log("when up arrow key is pressed,X="+player_x+",Y="+player_y);
+        canvas.remove(player_object);
+        player_update();
     }
 }
